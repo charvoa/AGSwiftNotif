5e893b3aad15d3d5e082afdc0918cbdc7c7469d3
Nicolas Charvoz <nicolas.charvoz@epitech.eu>
2016-09-28 15:24:01 +0200

    Swift3 updated

--- Pod/Classes/AGSwiftNotif.swift
+++ Pod/Classes/AGSwiftNotif.swift
@@ -1,32 +1,40 @@
+//
+//  AGSwiftNotif.swift
+//  Partying
+//
+//  Created by Nicolas on 28/09/2016.
+//  Copyright © 2016 Nicolas Charvoz. All rights reserved.
+//
+
 import Foundation
 
 public class AGSwiftNotif {
-    
+
     private var observerList = [AnyObject]()
-    
+
     public init(){
-        
+
     }
-    public func registerObserver(name:String!, object:AnyObject?=nil, completionHandler:(NSNotification!)->Void){
-        let observer = NSNotificationCenter.defaultCenter().addObserverForName(name, object: object, queue: nil, usingBlock: completionHandler)
-        
+    public func registerObserver(name:String!, object:AnyObject?=nil, completionHandler:@escaping (Notification!)->Void){
+        let observer = NotificationCenter.default.addObserver(forName: Notification.Name(name), object: object, queue: nil, using: completionHandler)
+
         observerList.append(observer)
     }
-    
+
     public func postNotification(name:String!,infos:[NSObject:AnyObject]?=nil){
-        NSNotificationCenter.defaultCenter().postNotificationName(name, object: nil, userInfo: infos)
+        NotificationCenter.default.post(name: Notification.Name(name), object: nil, userInfo: infos)
     }
-    
-    public func registerGroupObserver(names:[String],object:AnyObject?=nil, completionHandler:(NSNotification!)->Void){
+
+    public func registerGroupObserver(names:[String],object:AnyObject?=nil, completionHandler:@escaping (Notification!)->Void){
         for name in names{
-            let observer = NSNotificationCenter.defaultCenter().addObserverForName(name, object: object, queue: nil, usingBlock: completionHandler)
+            let observer = NotificationCenter.default.addObserver(forName: Notification.Name(name), object: object, queue: nil, using: completionHandler)
             observerList.append(observer)
         }
     }
-    
+
     deinit{
         for observer in observerList{
-            NSNotificationCenter.defaultCenter().removeObserver(observer)
+            NotificationCenter.default.removeObserver(observer)
         }
         observerList = []
     }
